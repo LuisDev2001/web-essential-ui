9b19944865e17421b31207ddb7d6d94a36239604
LuisDev01Lab <luisqrja@gmail.com>
2024-09-15 13:25:45 -0500

    feat: Create EButton component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+  import EButton from './components/EButton.ce.vue'
 </script>
 
 <template>
   <div>
     <h1 class="text-3xl font-bold">Vue 3 + Vite + TypeScript</h1>
     <p class="text-lg text-gray-600">Vue 3 + Vite + TypeScript + Composition API + ESLint + Prettier</p>
+    <EButton size="md">
+      Hola mundo
+    </EButton>
   </div>
 </template>
---
+++ src/components/EButton.ce.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+  import { computed } from 'vue'
+
+  export interface Props {
+    rounded?: boolean
+    outlined?: boolean
+    size: 'sm' | 'md' | 'lg'
+  }
+
+  const {
+    rounded = false,
+    outlined = false,
+    size = 'md'
+  } = defineProps<Props>()
+
+  const emit = defineEmits<{
+    click: [event: Event]
+  }>()
+
+  const sizeClasses = computed(() => {
+    const classes = {
+      sm: 'py-3 px-5 text-sm',
+      md: 'py-4 px-7 text-base',
+      lg: 'py-4 px-9 text-lg'
+    }
+    return classes[size]
+  })
+</script>
+
+<template>
+  <button
+    class="text-neutral-50 rounded-md"
+    :class="[
+      sizeClasses,
+      {'rounded-[37px]': rounded},
+      !outlined ? 'bg-blue-500' : 'bg-transparent border border-blue-500'
+    ]"
+    @click="emit('click', $event)"
+  >
+    <slot></slot>
+  </button>
+</template>
