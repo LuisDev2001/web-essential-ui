a60882f9fa63818336f4c9e78ebb441ce50af19f
LuisDev01Lab <luisqrja@gmail.com>
2024-09-18 22:49:23 -0500

    feat: Add EIcon to export component and shadowRoot option in false

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,35 @@ import EButton from '@/components/EButton.ce.vue'
 import EAlert from '@/components/EAlert.ce.vue'
 import ERadio from '@/components/ERadio.ce.vue'
 import ECheckbox from '@/components/ECheckbox.ce.vue'
+import EIcon from '@/components/EIcon.ce.vue'
 import EInput from '@/components/EInput.ce.vue'
 import ESelect from '@/components/ESelect.ce.vue'
 import ETable from '@/components/ETable.ce.vue'
 
-export const EButtonElement = defineCustomElement(EButton)
-export const EAlertElement = defineCustomElement(EAlert)
-export const ERadioElement = defineCustomElement(ERadio)
-export const ECheckboxElement = defineCustomElement(ECheckbox)
-export const EInputElement = defineCustomElement(EInput)
-export const ESelectElement = defineCustomElement(ESelect)
-export const ETableElement = defineCustomElement(ETable)
+export const EButtonElement = defineCustomElement(EButton, {
+  shadowRoot: false
+})
+export const EAlertElement = defineCustomElement(EAlert, {
+  shadowRoot: false
+})
+export const ERadioElement = defineCustomElement(ERadio, {
+  shadowRoot: false
+})
+export const ECheckboxElement = defineCustomElement(ECheckbox, {
+  shadowRoot: false
+})
+export const EInputElement = defineCustomElement(EInput, {
+  shadowRoot: false
+})
+export const ESelectElement = defineCustomElement(ESelect, {
+  shadowRoot: false
+})
+export const ETableElement = defineCustomElement(ETable, {
+  shadowRoot: false
+})
+export const EIconElement = defineCustomElement(EIcon, {
+  shadowRoot: false
+})
 
 const registerEssentialComponents = () => {
   customElements.define('e-button', EButtonElement)
@@ -25,6 +43,7 @@ const registerEssentialComponents = () => {
   customElements.define('e-input', EInputElement)
   customElements.define('e-select', ESelectElement)
   customElements.define('e-table', ETableElement)
+  customElements.define('e-icon', EIconElement)
 }
 
 export default registerEssentialComponents
